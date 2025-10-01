3e8bc3a33b5406a173faa6097482a82870d2e1aa
aloksport <alok@sportainmentdesign.com>
2025-10-01 15:00:08 +0530

    Update notes1.php

--- notes1.php
+++ notes1.php
@@ -2,3 +2,7 @@ Youtube video Idea
 https://tubelab.net/
 https://subscribr.ai/
 https://elevenlabs.io/text-to-speech
+
+
+DevOps Playlist
+https://www.youtube.com/watch?v=Ou9j73aWgyE&list=PLdpzxOOAlwvIKMhk8WhzN1pYoJ1YU8Csa
